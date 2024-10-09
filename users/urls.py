9055d68ffef254eaf4497f237b3095dc9d19c33e
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-10-09 11:53:07 +0100

    worked merge conflict

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD:ecommerce_api/users/urls.py
 from django.urls import path
 from .views import UserListCreateView, UserDetailView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view(), name='user-list-create'),
-    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path("", UserListCreateView.as_view(), name="user-list-create"),
+    path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
 ]
-=======
-# users/urls.py
-from django.urls import path
-from .views import UserListCreateView, UserDetailView
-
-urlpatterns = [
-    path('', UserListCreateView.as_view(), name='user-list-create'),
-    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-]
->>>>>>> d9dbb97d9946b0c67726c100f638085b28026333:users/urls.py
